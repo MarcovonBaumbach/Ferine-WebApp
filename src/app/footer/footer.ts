9e5c1a1d55d9856f51493231e93e788287ca8bbe
unknown <marco.vonbaumbach@gmx.de>
2025-09-22 14:21:24 +0200

    footer

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrls: ['./footer.scss']
 })
 export class FooterComponent {
-
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faFacebook, faInstagram, faYoutube, faEnvelope);
+  }
 }
