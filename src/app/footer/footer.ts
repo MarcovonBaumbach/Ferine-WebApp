29ca76856ade1e2589011bd64d4e745174cb3d1f
unknown <marco.vonbaumbach@gmx.de>
2025-09-22 14:43:42 +0200

    footer

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -12,7 +14,28 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./footer.scss']
 })
 export class FooterComponent {
-  constructor(private library: FaIconLibrary) {
+  currentRoute: string = '';
+
+  constructor(private library: FaIconLibrary, private router: Router) {
     library.addIcons(faFacebook, faInstagram, faYoutube, faEnvelope);
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.currentRoute = event.urlAfterRedirects;
+      });
+  }
+
+  getIconColor(icon: 'facebook' | 'instagram' | 'youtube'): string {
+    switch (this.currentRoute) {
+      case '/news':
+        return 'black';
+      case '/music':
+        return 'black';
+      case '/about':
+        return 'black';
+      default:
+        return 'white';
+    }
   }
 }
