db0bf909c0ed7e83bf2a838a5e4b7aa451edc087
unknown <marco.vonbaumbach@gmx.de>
2025-09-23 17:39:52 +0200

    imprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LandingPageComponent } from './landing-page/landing-page';
 import { AboutPageComponent } from './about-page/about-page';
 import { MusicPageComponent } from './music-page/music-page';
 import { NewsPageComponent } from './news-page/news-page';
+import { ImprintComponent } from './imprint/imprint';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
     {path: 'about', component: AboutPageComponent},
     {path: 'music', component: MusicPageComponent},
-    {path: 'news', component: NewsPageComponent}
+    {path: 'news', component: NewsPageComponent},
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
